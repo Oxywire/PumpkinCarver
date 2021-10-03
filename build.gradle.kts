9e460fef49aebe2c3824910ac700d76aa39d1d4b
SirSalad <sirsalad.mc@gmail.com>
2021-10-03 19:20:14 -0400

    Converted to Kotlin build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "com.oxywire"
+version = "1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.citizensnpcs.co/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly("net.citizensnpcs:citizensapi:2.0.28-SNAPSHOT")
+    implementation("cloud.commandframework:cloud-paper:1.5.0")
+}
+
+tasks {
+    shadowJar {
+        listOf(
+            "cloud.commandframework",
+            "io.leangen.geantyref"
+        ).forEach { relocate(it, "com.oxywire.pumpkincarver.libs.$it") }
+        minimize()
+        archiveFileName.set("PumpkinCarver.jar")
+    }
+
+    jar {
+        archiveFileName.set("PumpkinCarver-noshade.jar")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    processResources {
+        expand("version" to project.version)
+    }
+}
